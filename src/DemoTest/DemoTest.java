b5b369255c577970deb7077bcf57163e133f4f5f
GovindPatil75 <mulegr75@gmail.com>
2024-09-16 08:24:49 +0530

    Batch 28

---
+++ src/DemoTest/DemoTest.java
@@ -0,0 +1,39 @@
+package DemoTest;
+
+public class DemoTest {
+
+	public static void main(String[] args) {
+		
+		//datatype variblename=value;
+		int a=10;
+		System.out.println(a);
+		
+		float f=1.5f;
+		System.out.println(f);
+		
+		Double d=12.9;
+		System.out.println(d);
+		
+		char c='T';
+		System.out.println(c);
+		
+		boolean status=true;
+		System.out.println(status);
+		
+		// Deafult values
+		
+		int b = 0;
+		System.out.println(b);
+		
+		boolean y = false;
+		System.out.println(y);
+		
+		char u = 0;
+		System.out.println(u);
+		
+		double g = 0;
+		System.out.println(g);
+		
+	}
+
+}
